a3df0b14d017205624a1155152fd14bc6adeaa91
Jennifer Sieber <jenwebdev2@gmail.com>
2024-12-17 13:04:26 -0700

    Install Nextjs and TailwindCSS and create first template.

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500">Home</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa laboriosam accusantium saepe nulla quam iusto dolore perferendis rerum repudiandae repellat!</p>
+    </div>
+  )
+}
