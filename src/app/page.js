1cc3e4c7c162aa215d4b71cd3ddf4cd3785cfbd4
Jennifer Sieber <jenwebdev2@gmail.com>
2024-12-18 10:56:57 -0700

    Update Clerk sync and metadata info

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-500">Home</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa
-        laboriosam accusantium saepe nulla quam iusto dolore perferendis rerum
-        repudiandae repellat!
-      </p>
+      <h1 className="text-red-500">Welcome Home</h1>
+      <p>Wild Rose is under construction.</p>
     </div>
   );
 }
